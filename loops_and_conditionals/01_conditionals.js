6ea26740e9f276728b11ae2a174f98d1cf2dda7e
Armando Rojas <armando.rojas@colmena.cl>
2021-12-22 20:17:34 -0300

    Resolviendo conditional tests

--- loops_and_conditionals/01_conditionals.js
+++ loops_and_conditionals/01_conditionals.js
@@ -1,39 +1,10 @@
 // Ejemplo de if, else if
 function isBenefitsAssigned(salary, years) {
-  // Refactorizar esta lógica para que quede lo más legible y simple posible
-  if (salary < 400000) {
-    return true
-  } else if (salary >= 400000) {
-    if (salary > 500000) {
-      // Al parecer esta lógica está en negativo. ¡Piensa positivo!
-      if (years < 50) {
-        return false
-      } else {
-        return true
-      }
-    } else {
-      return false
-    }
-  } else {
-    return false
-  }
-
-  /*
     var isBasicIncome = salary < 400000
     var isMediumIncome = salary >= 400000 && salary < 500000
     var isOld = years >= 50
-  
-    if (isBasicIncome) {
-      return true
-    } else if (isMediumIncome && isOld) {
-      return true
-    } else {
-      return false
-    }
-  
-  */
-  // Aún más:
-  // return isBasicIncome || isMediumIncome && isOld
+
+    return isBasicIncome || isMediumIncome && isOld
 }
 // Ejemplo de switch-case
 function getMessageByHttpStatusCode(code) {
@@ -47,8 +18,10 @@ function getMessageByHttpStatusCode(code) {
   switch(code) {
     case 200:
       message = 'OK'
+      break
     case 201:
       message = 'Created'
+      break
     case 301:
       message = 'Moved Permanently'
       break
@@ -58,8 +31,11 @@ function getMessageByHttpStatusCode(code) {
     case 500:
       message = 'Internal Server Error'
       break
+    default:
+      message = 'Invalid Code Status'
+      break
   }
-  return message 
+  return message
 
   // switch(code) {
   //   case 200:
@@ -76,23 +52,23 @@ function getMessageByHttpStatusCode(code) {
 }
 // Ejemplo de try-catch
 function sendEmail(address) {
-  //try {
+  try {
     if(address) {
       // ... deliverMailMethod(address)
       return 'Email successfully delivered'
     } else {
       throw new Error('Email not delivered')
     }
-  //} catch (error) {
-  //  return error.message
-  //}
+  } catch (error) {
+    return error.message
+  }
 }
 // Ejemplos de valores 'falsy'
 function isFalsy() {
   // https://developer.mozilla.org/en-US/docs/Glossary/Falsy
-  var value = true
+  //var value = true
 
-  // value = false
+  //value = false
   // value = 0
   // value = -0
   // value = ''
@@ -103,21 +79,21 @@ function isFalsy() {
   // value = [].length
 
   // En lo posible es mejor ser explicito
-  // value = 0
-  // if(!value){
-  //   value = false
-  // }
-    
+   var value
+   if(!value){
+     value = false
+   }
+
     return value
 }
 // Ejemplo de ternario
 function getDrink(age) {
-  if(age <= 18) {
+  /* if(age >= 18) {
     return 'Cerveza'
   } else{
     return 'Jugo'
-  }
-  // return (age >= 18) ? 'Cerveza' : 'Jugo'
+  } */
+  return (age >= 18) ? 'Cerveza' : 'Jugo'
 }
 
 module.exports = {
